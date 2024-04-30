eeab3613d00fb8d66c6ebfa444b20670c60719de
azizx018 <chiara.aziz@gmail.com>
2024-04-30 12:58:54 -0500

    adding initial styling

--- my-stories/app/page.tsx
+++ my-stories/app/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import BookLogo from "./ui/book-logo";
+import styles from '@/app/ui/home.module.css';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-black-500 p-4 md:h-52">
         <BookLogo/>
       </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className={styles.shape}
+        />
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
