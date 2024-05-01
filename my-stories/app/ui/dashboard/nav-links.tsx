a7990aff5670326a29c32793f39a190a48f2be2f
azizx018 <chiara.aziz@gmail.com>
2024-05-01 13:16:19 -0500

    nav bar styling

--- my-stories/app/ui/dashboard/nav-links.tsx
+++ my-stories/app/ui/dashboard/nav-links.tsx
@@ -8,7 +8,7 @@ import clsx from 'clsx';
 //Map the links to dispaly them in the sidNav
 
 const links = [
-    {name: 'Home', href: '/dashboard', icon: HomeIcon},
+    { name: 'Home', href: '/dashboard', icon: HomeIcon },
     {
         name: 'My Books',
         href: '/dashboard/my-books',
@@ -25,16 +25,16 @@ export default function NavLinks() {
                 const LinkIcon = link.icon;
                 return (
                     <Link
-                      key={link.name}
-                      href={link.href}
-                      className={clsx(
-                        'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-                        {
-                            'bg-sky-100 text gray-600': pathname === link.href,
-                        },
-                      )}
+                        key={link.name}
+                        href={link.href}
+                        className={clsx(
+                            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-black-50 p-3 text-sm font-medium hover:bg-gray-100 hover:text-gray-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                            {
+                                'bg-black-100 text gray-600': pathname === link.href,
+                            },
+                        )}
                     >
-                        <LinkIcon className="w-6"/>
+                        <LinkIcon className="w-6" />
                         <p className="hidden md:block">{link.name}</p>
                     </Link>
                 );
