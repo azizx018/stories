dc6055e76c5b6be35f5b5234e835ebc7d7abf8b6
azizx018 <chiara.aziz@gmail.com>
2024-05-01 14:46:18 -0500

    added definitions

---
+++ my-stories/app/lib/definitions.ts
@@ -0,0 +1,15 @@
+//type definitions for data
+
+export type User = {
+    id: string,
+    name: string,
+    email: string, 
+    password: string
+};
+
+export type Books = {
+    id: string,
+    title: string,
+    author: string,
+    cover_image: string
+}
